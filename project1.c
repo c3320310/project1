9f6045f45ca2bbd9c7d1e44d8cc14051eec8b363
Jacob Jenkinson <jacobjenknison@live.com.au>
2019-04-03 08:21:49 +0000

    i have ascii numbers to letters

---
+++ project1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+
+    char str[1024]= "happy projecting";    // string inside " ";
+    char c = 53;                    // ascii number 53 is output 5
+        printf("read: %s\n", str);  // %s reads strings
+        printf("ascii %c\n", c);      // %c reads ascii
+
+int i;
+    for ( i= 97; i< 123; i++){       
+    printf("%c", i);                //ascii for lower case
+    }                               
+  printf("\n");                    
+    for (i=65;i<91;i++){            // ascii lower case
+    printf("%c", i);                
+    }
+
+return 0;
+}
+
+/*
+meun
+1 encript with rotation with text and key shift
+2 decript rotation "   "
+3 encript with substitution text and sub
+4 decript substitution      "    "
+5 decript rotation text only
+6 decript substitution text only
+*/
